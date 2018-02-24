5049f315dcd727ee580103d5dbe8d353a14be2bf
Gua <727095319@qq.com>
2018-02-24 09:50:25 +0800

    1.0.1
    1.0.1

--- js/gz-paging.js
+++ js/gz-paging.js
@@ -1,4 +1,252 @@
 /**
+ * 分页
+ * 
+ * @param {number}  num       在一页中显示的数量（默认为10） 
+ * @param {object}  data      在发送请求的时候同时传入的数据
+ * @param {string}  url       获取列表信息的地址
+ * @param {string}  key       返回数据中信息列表的键名
+ * @param {string}  pagNumber 分页显示个数上限
+ * @param {boolean} webpaging 是否采用服务器分页，否则使用前端分页(使用后key无效)
+ * @function reload           当前页数的列表重新获取,参数为所需跳转的页数,不填写为刷新当前页
+ * @function changeId         表格内数据重新更换,参数为新表格变更的新设置
+ * @function getTotal         返回总页数
+ */
+
+function setPage(o) {
+    var _page = 1, //当前页
+        _shownum = 10, // 一页显示的数量
+        _totalnum, // 总条目数
+        _maxpage, // 页面总数
+        _data, // 数据
+        _pageElement, // 分页所在节点
+        _dataUrl = o.url,
+        _key = "rows",
+        _pageTotal = 5,
+        _hasAddEvent = false
+
+    if (o.hasOwnProperty("num")) {
+        _shownum = o.num
+    }
+
+    if (o.hasOwnProperty("key")) {
+        _key = key
+    }
+
+    if (o.hasOwnProperty('pagNumber')) {
+        _pageTotal = o.pagNumber
+    }
+
+    first()
+    // 初始化显示
+    function first() {
+        // 设置分页节点
+        setDom()
+        
+        // 找到所在节点
+        _pageElement = $(".gz-page_box").find('.gz-paging')
+        if (o.hasOwnProperty('pager')) {
+            _pageElement = $(o.pager + ' .gz-paging')
+        }
+
+        // 获取数据
+        getData()
+    }
+
+    // 分页DOM创建
+    function setDom() { 
+        var _box = $('<div class="gz-paging"></div>'),
+            _button = $('<p class="button"></p>'),
+            _pagenum = $('<div class="gz-pagenum"></div>')
+            _prev = _button.clone().addClass('prev').text('上一页'),
+            _next = _button.clone().addClass('next').text('下一页'),
+            _total = $('<p class="gz-total">总页数:<span></span>页</p>')    
+        _box.append(_prev, _pagenum, _next,_total)
+        $('.gz-page_box').append(_box)
+    }
+
+    // 打印数据 
+    function printTemplate(r) {
+        var res = template(o.tp, {
+            'list': r,
+            'start': (_page - 1) * _shownum
+        })
+        $(o.el).html(res)
+        return r
+    }
+    // 获取数据
+    function getData() {
+        var option = {
+            url: _dataUrl,
+            type: 'post'
+        },
+            data = {}
+        for (var key in o.data) {
+            if (o.data.hasOwnProperty(key)) {
+                data[key] = o.data[key]
+            }
+        }
+        option.data = data
+        if (o.webpaging === null || o.webpaging === undefined) {
+            // 使用服务器分页
+            option.success = function (d) {
+                _totalnum = d.total
+                _maxpage = Math.ceil(_totalnum / _shownum)
+                $(_pageElement).find('.gz-total span').html(_maxpage)
+                if (!_hasAddEvent) {
+                    addButtonEvent()
+                    _hasAddEvent = true
+                }
+                _data = d[_key]
+                printTemplate(_data)
+            }
+            option.data.row = _shownum
+            option.data.page = _page
+        
+            setAjax(option)
+        } else if(o.webpaging) {
+            // 使用前端分页
+            if (!_data) {
+                option.success = function (d) {
+                    _totalnum = d.length
+                    _maxpage = Math.ceil(_totalnum / _shownum)
+                    $(_pageElement).find('.gz-total span').html(_maxpage)
+                    if (!_hasAddEvent) {
+                        addButtonEvent()
+                        _hasAddEvent = true
+                    }
+                    _data = d
+                    printTemplate(_data.slice((_page - 1) * _shownum, _page * _shownum))
+                }
+                setAjax(option)
+            } else {
+                printTemplate(_data.slice((_page - 1) * _shownum, _page * _shownum))
+            }
+        }
+    }
+
+    // 下一页
+    function next() {
+        if (_page + 1 > _maxpage) {
+            return false
+        }
+        _page = (_page + 1) > _maxpage ? _maxpage : _page + 1
+
+    }
+
+    // 上一页
+    function prev() {
+        if (_page - 1 < 1) {
+            return false
+        }
+        _page = (_page - 1) < 1 ? 1 : _page - 1
+    }
+
+    // 事件添加
+    function addButtonEvent() {
+        setPager()
+        _pageElement.click(function (e) {
+            var e = e || window.event,
+                el = e.target,
+                $el = $(e.target)
+            if ($el.hasClass('button') || 　el.tagName == "P") {
+                if ($el.hasClass('next')) {
+                    next()
+                } else if ($el.hasClass('prev')) {
+                    prev()
+                } else if (el.tagName == "P") {
+                    if (Number($el.attr('data-page')) != _page) {
+                        _page = Number($el.attr('data-page'))
+                    } else {
+                        return false
+                    }
+                }
+                setPager() 
+                getData()
+            } else {
+                return false
+            }
+        })
+    }
+
+    // 设置数字显示
+    function setPager() {
+        var el = _pageElement.find('.gz-pagenum'),
+            _middleIndex = Math.floor(_pageTotal / 2),
+            i
+        el.html("")
+        if (_maxpage <= _pageTotal) {
+            for (i = 1; i <= _maxpage; i++) {
+                el.append("<p data-page='" + i + "'>" + i + "</p>")
+            }
+        } else {
+            var end
+            if (_page > _middleIndex && _pageTotal + _page - _middleIndex <= _maxpage + 1) {
+                i = _page - _middleIndex
+                end = _pageTotal + _page - _middleIndex
+            } else if (_page <= _middleIndex) {
+                i = 1
+                end = _pageTotal + 1
+            } else if (_maxpage - _page <= _middleIndex) {
+                i = _maxpage - _pageTotal + 1
+                end = _maxpage + 1
+            }
+            for (i ; i < end; i++) {
+                el.append("<p data-page='" + i + "'>" + i + "</p>")
+            }
+        }
+
+        el.find('p').removeAttr('class').siblings('p[data-page=' + _page + ']').addClass('active')
+    }
+
+    // 摧毁现有分页节点
+    function destroyPaging() { 
+        $(".gz-paging").remove()
+    }
+
+    return {
+        // 更改数据重新刷新
+        changeId: function (oinfo) {
+            destroyPaging()
+
+            if (!o.hasOwnProperty("data")) {
+                o.data = {}
+            }
+
+            for (var key in oinfo) {
+                if (oinfo.hasOwnProperty(key)) {
+                    if (key == "url") {
+                        o.url = oinfo.url
+                    } else {
+                        o.data[key] = oinfo[key];
+                    }
+                }
+            }
+            
+            setPage(o)
+        },
+        // 当前页数表格刷新
+        reload: function (number) {
+            destroyPaging()
+            if (number != null) {
+                _page = number
+            }
+            getData(o)
+        },
+        // 获取总页数
+        getTotal: function () {
+            return _maxpage
+        }
+    }
+}
+
+if (window.hasOwnProperty("template")) {
+    template.defaults.imports.getTime = function (d) {
+        return moment(d).format('L')
+    }
+}
+
+
+/**
  * 封装ajax请求
  * 
  * @param {object} o 
@@ -18,15 +266,12 @@ function setAjax(o) {
         url: o.url,
         data: o.data,
         type: t,
-        dataType:'json',
+        dataType: 'json',
         success: function (d) {
             o.success(d)
         },
         error: function () {
-            $.gzMaskLayer({
-                word: 'Hey 哥们，是哪里出错了 :(',
-            }).error()
-            console.log('HEY GUYS,AJAX ERROR :(    URL IS ' + o.url)
+            console.warn('HEY GUYS,AJAX ERROR :(    URL IS ' + o.url)
         }
     }
     if (o.hasOwnProperty('upfile')) {
@@ -39,190 +284,21 @@ function setAjax(o) {
 
 
 /**
- * 分页
+ *  MADE BY gz
+ *  github:www.github.com/zhaoweihan2015
+ *  URL:www.welush.com
  * 
- * @param {any} d 传入的数据 
- * @param {number} num 在一页中显示的数量（默认为10） 
- * @param {object} data 在发送请求的时候同时传入的数据
- * @param {string} url 获取列表信息的地址
- * @param {string} total 获取列表总数的地址
- * @function reload 当前页数的列表重新获取
- * @function changeId 表格内数据重新更换,参数为新表格变更的新设置
+ *     00000000
+ *    00      00 
+ *    00  
+ *    00
+ *    00   00000 
+ *    00     100111111    
+ *    00     100   11
+ *     00000000   11
+ *               11
+ *              11     
+ *             11    
+ *            11     1
+ *           111111111
  */
-
-function setPage(o) {
-    var _page = 1,//当前页
-        _shownum = 10, // 一页显示的数量
-        _totalnum, // 总条目数
-        _maxpage, // 页面总数
-        _data, // 数据
-        _pageElement = o.pager, // 分页所在节点
-        _dataUrl = o.url,
-        _totalUrl = o.total
-            
-    if (o.hasOwnProperty("num")) {
-        _shownum = o.num 
-    }
-
-    
-
-    first()
-    // 初始化显示
-    function first() {
-        getTotal()
-        getData()
-    }
-
-    // 获取总数
-    function getTotal() { 
-    	var option = {
-                url: _totalUrl,
-                success: function (d) { 
-                    _totalnum = d
-                    _maxpage = Math.ceil(_totalnum / _shownum)
-                    $(_pageElement).find('.gz-total span').html(_maxpage)
-                    pager()
-                }
-        }
-        option.data = {}
-            for (var key in o.data) {
-                if (o.data.hasOwnProperty(key)) {
-                option.data[key] = o.data[key]
-            }
-        }
-        setAjax(option)
-    }
-
-    // 打印数据
-    function printTemplate(r) {
-        var res = template(o.tp, {
-            'list': r,
-            'start': (_page-1)*_shownum
-        })
-        $(o.el).html(res)
-        return r
-    }
-
-    // 获取数据
-    function getData() {
-    	var option = {
-                url: _dataUrl,
-                type:'post',
-                success: function (d) {
-                    printTemplate(d)
-                }
-            },
-            data = {
-                row: _shownum,
-                page: _page
-            }
-        for (var key in o.data) {
-            if (o.data.hasOwnProperty(key)) {
-                data[key] = o.data[key]
-            }
-        }
-    	option.data = data
-        setAjax(option)
-    }
-
-    // 下一页
-    function next() {
-        if (_page + 1 > _maxpage) {
-            return false
-        }
-        _page = (_page + 1) > _maxpage ? _maxpage : _page + 1
-        getData(o)
-    }
-
-    // 上一页
-    function prev() {
-        if (_page - 1 < 1) {
-            return false
-        }
-        _page = (_page - 1) < 1 ? 1 : _page - 1
-        getData(o)
-    }
-
-    // 事件添加
-    function pager() { 
-        setPager()
-        $(_pageElement).find('.prev').click(function(){
-            prev()
-            setPager()
-        })
-
-        $(_pageElement).find('.next').click(function(){
-            next()
-            setPager()
-        })
-    }
-
-    // 设置数字显示
-    function setPager() { 
-        var _pageTotal = 5,
-            el = $(_pageElement).find('.gz-pagenum')
-        if (_maxpage <= _pageTotal) {
-            el.html("")
-            for (var i = 1; i <= _maxpage; i++) {
-                el.append("<p data-page='" + i + "'>"+i+"</p>")
-            }
-        } else {
-            if (_page > 2 && _pageTotal + _page -2 <= _maxpage +1) {
-                el.html("")
-                for (var i = _page-2; i < _pageTotal+_page-2; i++) {
-                    el.append("<p data-page='" + i + "'>"+i+"</p>")
-                }
-            } else if(_page <= 2) {
-                el.html("")
-                for (var i = 1; i <= _pageTotal; i++) {
-                    el.append("<p data-page='" + i + "'>"+i+"</p>")
-                }
-            } else if (_maxpage - _page <= 2) {
-                el.html("")
-                for (var i = _maxpage-_pageTotal+1; i <= _maxpage; i++) {
-                    el.append("<p data-page='" + i + "'>"+i+"</p>")
-                }
-            }
-        }
-
-        el.find('p').removeAttr('class').click(function () { 
-            _page = Number($(this).attr('data-page'))
-            setPager()
-            getData(o)
-        }).siblings('p[data-page=' + _page + ']').addClass('active')
-    }
-
-    return {
-        // 更改数据重新刷新
-        changeId: function (oinfo) { 
-            if (!o.hasOwnProperty("data")) {
-                o.data = {}
-            }
-            for (var key in oinfo) {
-                if (oinfo.hasOwnProperty(key)) {
-                    if (key == "url") {
-                        o.url = oinfo.url
-                    } else if (key == "total") {
-                        o.total = oinfo.total
-                    } else {
-                        o.data[key] = oinfo[key];
-                    }
-                }
-            }
-            setPage(o)
-        },
-        // 当前页数表格刷新
-        reload: function (number) {
-            if (number != null) {
-                _page = number
-            }
-        	getData(o)
-        }
-    }
-}
-
-if (window.hasOwnProperty("template")) {
-    template.defaults.imports.getTime = function(d){ 
-        return moment(d).format('L')
-    }
-}
